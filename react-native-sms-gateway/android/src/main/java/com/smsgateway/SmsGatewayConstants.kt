197977026b680c27fd91f41e0e5204bb878aa251
hildayatul084-cloud <hildayatul084@gmail.com>
2026-06-08 05:53:49 +0700

    Delete react-native-sms-gateway directory

--- react-native-sms-gateway/android/src/main/java/com/smsgateway/SmsGatewayConstants.kt
+++
@@ -1,8 +0,0 @@
-package com.smsgateway
-
-object SmsGatewayConstants {
-    const val TAG = "SmsGateway"
-    const val SmsEvent = "com.smsgateway.SmsGatewayModule"
-    const val NAME = "SmsGateway"
-    const val ConfigName = "SmsGatewayConfig"
-}
